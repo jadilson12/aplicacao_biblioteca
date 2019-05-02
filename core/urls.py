926c8d70ee96a63150857a0191eb7b9535e6b851
Jadilson Guedes <jadilson12@gmail.com>
2019-05-02 11:37:47 -0300

    create: adiciona livro usando modal

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from core import views as v
 urlpatterns = [
     path('', v.home, name='core_home'),
     path('livros/', v.lista_livro, name='core_livros'),
-
+    path('livros/novo', v.novo_livro, name='core_novo_livro'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.forms import LivroForm
 from core.models import Livro
 
@@ -10,6 +10,12 @@ def home(resquest):
 def lista_livro(request):
     livros = Livro.objects.all()
     form = LivroForm()
-    data = {'livros': livros,  'form': form}
+    data = {'livros': livros, 'form': form}
     return render(request, 'core/livros.html', data)
 
+
+def novo_livro(request):
+    form = LivroForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('core_livros')
