471db1fe027f752a694e667b1ae08fb844cfdec3
Jadilson Guedes <jadilson12@gmail.com>
2019-05-02 10:39:49 -0300

    admin: adiconado o acesso para usar a models  livros e categoria

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from  .models import(
+    Categoria,
+    Livro
 
-# Register your models here.
+)
+
+
+admin.site.register(Categoria)
+admin.site.register(Livro)
