85e0e3e4fc1d13cdbf9b98064184eef8f67b5236
Jadilson Guedes <jadilson12@gmail.com>
2019-05-02 10:38:51 -0300

    models: adicionado models livros e categoria

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.nome
+
+
+class Livro(models.Model):
+    nome = models.CharField(max_length=50)
+    autor = models.CharField(max_length=50)
+    editora = models.CharField(max_length=50)
+    lancamento = models.DateField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome + ' - ' + self.autor
