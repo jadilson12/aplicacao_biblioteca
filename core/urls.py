edbb960a338d270fa48102b482a419014708eaee
Jadilson Guedes <jadilson12@gmail.com>
2019-05-05 22:14:59 -0300

    view: adiciona funcionalidades  para o modal
    * Salvar cadastro
    * Deletar Cadastro
    * Lista Cadastro

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,11 @@ from core import views as v
 urlpatterns = [
     path('', v.home, name='core_home'),
     path('livros/', v.lista_livro, name='core_livros'),
-    path('livros/novo', v.novo_livro, name='core_novo_livro'),
+
+    # api
+    path('api/livros', v.livroApiList, name='livrosApiList'),
+    path('api/livros/store', v.livro_store, name='livros_store'),
+    path('api/livros/<int:id>/', v.livro_detail, name='livroApiDetail'),
+    path('api/livros/update/<int:id>/', v.livro_update, name='livro_update'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 from core.forms import LivroForm
 from core.models import Livro
+from core.serializers import LivroSerializer
 
 
 def home(resquest):
@@ -11,11 +16,126 @@ def lista_livro(request):
     livros = Livro.objects.all()
     form = LivroForm()
     data = {'livros': livros, 'form': form}
-    return render(request, 'core/livros.html', data)
+    return render(request, 'core/livros_lista.html', data)
 
 
-def novo_livro(request):
-    form = LivroForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    return redirect('core_livros')
+class JSONResponse(HttpResponse):
+    """
+    O HttpResponse que renderiza seu conteúdo em JSON.
+
+    """
+
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+    class JSONResponse(object):
+        pass
+
+
+@csrf_exempt
+def livroApiList(request):
+    """
+    Lista de livros e criar novo livro
+
+    """
+    if request.method == 'GET':
+        livro = Livro.objects.all()
+        serializer = LivroSerializer(livro, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+
+        data = JSONParser().parse(request)
+        serializer = LivroSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def livro_store(request):
+
+    """
+    Salvar livro usando api
+
+    """
+    # abort if request is not made via POST
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(['POST'])
+
+    # abort if request is not made via ajax
+    if not request.is_ajax():
+        error = {'error': 'This resource requires ajax requests.'}
+        return JsonResponse(error, status=422)
+
+    # abort if posted data is not valid
+    form = LivroForm(request.POST)
+    if not form.is_valid():
+        error = {'error': form.errors}
+        return JsonResponse(error, status=422)
+
+    nome = form.cleaned_data.get('nome')
+    autor = form.cleaned_data.get('autor')
+    editora = form.cleaned_data.get('editora')
+    categoria = form.cleaned_data.get('categoria')
+    lancamento = form.cleaned_data.get('lancamento')
+    resumo = form.cleaned_data.get('resumo')
+    Livro.objects.create(
+        nome=nome,
+        autor=autor,
+        editora=editora,
+        categoria=categoria,
+        lancamento=lancamento,
+        resumo=resumo,
+
+    )
+    message = 'salvo.'
+    return JsonResponse({'message': message})
+
+
+def livro_detail(request, id):
+    """
+    Visualiza, atualiza e exclui livro
+
+    """
+    try:
+        livro = Livro.objects.get(id=id)
+    except Livro.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = LivroSerializer(livro)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = LivroSerializer(livro, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        livro.delete()
+        return HttpResponse(status=204)
+
+
+def livro_update(request, id):
+    if request.method == 'POST':
+        id = request.POST.get('livro_id')
+        livro = get_object_or_404(Livro, id=id)
+        form = LivroForm(request.POST, instance=livro)
+
+        if form.is_valid():
+            form.save()
+            response = {'status': 'atualizado'}
+            return JsonResponse(response)
+
+        error = {'error': form.errors}
+        return JsonResponse(error, status=422)
+    else:
+        response = ''
+        return JsonResponse(response, )
