ff8a00a3da0f17c46a9b013d4b858467cd2acdf1
Jadilson Guedes <jadilson12@gmail.com>
2019-05-30 12:52:43 -0300

    [UPDATE] Atualizando  book_services

--- library/services/book_services.py
+++ library/services/book_services.py
@@ -1,77 +1,46 @@
-from django.shortcuts import get_object_or_404, render
-from django.template.loader import render_to_string
-from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from library.forms import *
 from library.models import *
 
 
 def list(request):
     books = Book.objects.all()
-    data = {'books': books}
+    form = BookForm()
+    data = {'books': books, 'form': form}
     return render(request, 'book/book_list.html', data)
 
 
 def create(request):
-    form = BookForm(request.POST)
-    if not request.method == 'POST':
-        form = BookForm()
-    return save(request, form, 'book/book_create.html')
-
-
-def save(request, form, template_name):
-
-    # create one dictionary for send to json
-    data = dict()
-
     if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-
-            # send  feedback for notify to the user
-            data['form_is_valid'] = True
-            books = Book.objects.all()
-
-            # add the list  tr in the dictionary
-            data['book_list'] = render_to_string('book/book_list_tr.html', {'books': books})
-        else:
-            # send  feedback for notify to the user
-            data['form_is_valid'] = False
-
-    # add form if is valid or not
-    context = {'form': form}
-
-    # return to front new list updated
-    data['html_form'] = render_to_string(template_name, context, request=request)
-    return JsonResponse(data)
+            return redirect('book.list')
+    else:
+        form = BookForm()
+        return render(request, 'book/book_update.html', {'form': form})
 
 
 def update(request, id):
-    # search the object to be updated
-    books = get_object_or_404(Book, id=id)
+    data = {}
+    book = Book.objects.get(id=id)
+    form = BookForm(request.POST, request.FILES, instance=book)
+    data['book'] = book
+    data['form'] = form
+    data['category'] = Category.objects.all()
+    print(data)
     if request.method == 'POST':
-        # update
-        form = BookForm(request.POST, instance=books)
+        if form.is_valid():
+            form.save()
+            return redirect('book.list')
     else:
-        # not updated
-        form = BookForm(instance=books)
-    return save(request, form, 'book/book_update.html')
+        return render(request, 'book/book_update.html', data)
 
 
 def delete(request, id):
-    data = dict()
-    # search the object to be deleted
-    books = get_object_or_404(Book, id=id)
-    if request.method == "DELETE":
-        books.delete()
-
-        # send  feedback for notify to the user
-        data['form_is_valid'] = True
-        books = Book.objects.all()
-
-        # return to front new list updated
-        data['book_list'] = render_to_string('book/book_list_tr.html', {'books': books})
+    book = Book.objects.get(id=id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book.list')
     else:
-        context = {'books': books}
-        data['html_form'] = render_to_string('book/book_delete.html', context, request=request)
-
-    return JsonResponse(data)
+        return render(request, 'book/book_create.html')
