20f0b5baca53d33f69b4a8a5b882214356a3fc62
Jadilson Guedes <jadilson12@gmail.com>
2019-05-30 08:16:24 -0300

    [UPDATE] Atualiza caminho para o uso das imagens

--- booksfind/settings.py
+++ booksfind/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'home'
